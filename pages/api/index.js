9e4ce3aedcfefce034883a342bd6eda25ce9df16
Alpha Abelandrianarivo <abelaalpha@gmail.com>
2023-02-20 13:30:28 +0300

    new file:   .env 	new file:   back-openai 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/_app.js 	deleted:    pages/api/hello.js 	new file:   pages/api/index.js 	modified:   pages/index.js 	new file:   postcss.config.js 	new file:   public/logo.png 	deleted:    styles/Home.module.css 	modified:   styles/globals.css 	new file:   tailwind.config.js

---
+++ pages/api/index.js
@@ -0,0 +1,17 @@
+import { ChatGPTAPI } from 'chatgpt';
+
+const api = new ChatGPTAPI({
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+
+export default async function handler(req, res) {
+  const { question } = JSON.parse(req.body);
+  console.log(question);
+
+  if (question) {
+    const response = await api.sendMessage(question)
+    console.log(response);
+    res.status(200).json({data :response.text})
+  }
+}
