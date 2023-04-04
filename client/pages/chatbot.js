0a9e8ccd73cc71ee5c5f5c6bdbaa3ee64369a57f
Alpha Abelandrianarivo <abelaalpha@gmail.com>
2023-04-04 19:58:32 +0300

    modified:   client/pages/chatbot.js 	modified:   client/pages/index.js

--- client/pages/chatbot.js
+++ client/pages/chatbot.js
@@ -91,7 +91,8 @@ export default function ChatBot() {
           </div>
           
         </div>
-        <div className=' flex bg-slate-900 border-b justify-between items-center w-full h-10 p-5'/>
+        <div className=' flex bg-slate-900 border-b justify-between items-center w-full h-10 p-5' />
+        <div className='w-full h-10 p-5' />
 
         {/* Conversation */}
         <div className='h-full pb-5'>
